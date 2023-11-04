3ddef9d3616575dd5526b159bbaf0ba123953891
caprav <vines07@gmail.com>
2023-11-03 20:05:25 -0400

    Implements query_is_primary_song_artist and moves return query out of run.py. Functionality preserved.

--- run.py
+++ run.py
@@ -1,36 +1,34 @@
 # sk-Import necessary libraries and modules
 import os
-from   flask_migrate import Migrate
-from   flask_minify  import Minify
-from   sys import exit
+from flask_migrate import Migrate
+from flask_minify import Minify
+from sys import exit
 from rdflib import Graph, Namespace, Literal
 from rdflib.graph import DATASET_DEFAULT_GRAPH_ID as default
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 from SPARQLWrapper import SPARQLWrapper, JSON, N3
 from pprint import pprint
-from two_hit_wonders import external_ontology_queries  #  query_dbpedia_isHitSongOf, query_foaf_artist_name
+from two_hit_wonders import (
+    external_ontology_queries, return_all_isHitSongOf_artists
+)  #  query_dbpedia_isHitSongOf, query_foaf_artist_name
 from flask import Flask, render_template, request
 from apps.config import config_dict
 from apps import create_app, db
-import jsonify 
-
-
- 
+import jsonify
 
 
 # WARNING: Don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # The configuration
-get_config_mode = 'Debug' if DEBUG else 'Production'
+get_config_mode = "Debug" if DEBUG else "Production"
 
 try:
-
     # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
 
 except KeyError:
-    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+    exit("Error: Invalid <config_mode>. Expected values [Debug, Production] ")
 
 app = create_app(app_config)
 Migrate(app, db)
@@ -38,29 +36,22 @@ Migrate(app, db)
 if not DEBUG:
     Minify(app=app, html=True, js=False, cssless=False)
 
-    
- 
+
 if DEBUG:
-    app.logger.info('DEBUG            = ' + str(DEBUG)             )
-    app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
-    app.logger.info('DBMS             = ' + app_config.SQLALCHEMY_DATABASE_URI)
-    app.logger.info('ASSETS_ROOT      = ' + app_config.ASSETS_ROOT )
-
-
+    app.logger.info("DEBUG            = " + str(DEBUG))
+    app.logger.info("Page Compression = " + "FALSE" if DEBUG else "TRUE")
+    app.logger.info("DBMS             = " + app_config.SQLALCHEMY_DATABASE_URI)
+    app.logger.info("ASSETS_ROOT      = " + app_config.ASSETS_ROOT)
 
 
 # Sharayu: Define a route to interact with Fuseki and DBpedia
-@app.route('/get_fuseki',methods =['POST'])
-
+@app.route("/get_fuseki", methods=["POST"])
 def fuseki():
     print("inside fuseki")
 
-    #query1= request.form['query']
+    # query1= request.form['query']
 
-    
-
-# Sharayu: Define SPARQL query to retrieve data from DBpedia
-
+    # Sharayu: Define SPARQL query to retrieve data from DBpedia
 
     sparql = SPARQLWrapper("https://dbpedia.org/sparql")
     sparql.setQuery(
@@ -74,27 +65,12 @@ def fuseki():
     """
     )
     sparql.setReturnFormat(JSON)
-    qres = sparql.query().convert()
+    #qres = sparql.query().convert()
 
     store = SPARQLUpdateStore()
     query_endpoint = "http://localhost:3030/music/query"
     update_endpoint = "http://localhost:3030/music/update"
     store.open((query_endpoint, update_endpoint))
-
-    query1 = ('SELECT distinct ?works ?artist WHERE {?works a dbo:Song; dbp:award "Platinum"@en; dbo:artist ?artist; '
-          'dbo:releaseDate ?releaseDate. FILTER( ?releaseDate > "2007-01-01"^^xsd:date && ?releaseDate < '
-          '"2007-02-01"^^xsd:date)}')
-    query2 = ('CONSTRUCT {?works <http://example.org/isHitSongOf> ?artist} WHERE {?works a dbo:Song; dbp:award '
-          '"Platinum"@en; dbo:artist ?artist; dbo:releaseDate ?releaseDate. FILTER( ?releaseDate > '
-          '"2007-01-01"^^xsd:date && ?releaseDate < "2007-02-01"^^xsd:date)}')
-    query3 = ('PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '
-          'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> '
-          'PREFIX ex: <http://example.org/> '
-          'PREFIX foaf: <http://xmlns.com/foaf/0.1/> '
-          'SELECT DISTINCT ?artist_name WHERE { '
-          '?song ex:isHitSongOf ?artist. '
-          '?artist foaf:name ?artist_name '
-          '}')
 
 
     sparql = SPARQLWrapper("https://dbpedia.org/sparql")
@@ -107,37 +83,23 @@ def fuseki():
         query_result = sparql.query().convert()
         g.parse(query_result)
 
-     
-
-    
-
     #  VC - This line actually puts the info into Fuseki
     store.add_graph(g)
 
-    
-
     #  VC - can't use sparql below, need to use the store (pointing to Fuseki), sparql is pointing to dbpedia
-    fuseki_result = store.query(query3)
+    fuseki_result = store.query(return_all_isHitSongOf_artists)
 
-# sharayu - Process and print the results
-    res=""""""
+    # sharayu - Process and print the results
+    res = """"""
 
     for record in fuseki_result:
-        res+= (record.artist_name.toPython())
+        res += record.artist_name.toPython()
+
+    print(res)
+
+    return render_template("fedrated.html", query_result=res)
 
 
-    print(res)    
-
-
-    
-
-     
-
-
-    return render_template('fedrated.html', query_result=qres)
-
-  
 if __name__ == "__main__":
     # Change the port to the desired value (e.g., 8080)
     app.run()
-
--- two_hit_wonders.py
+++ two_hit_wonders.py
@@ -1,5 +1,6 @@
 # VC - a list of queries to execute external ontologies
 external_ontology_queries = []
+return_Fuseki_results_queries = []
 prefixes = "prefix foaf: <http://xmlns.com/foaf/0.1/>"
 
 """
@@ -8,6 +9,14 @@ VC - Here parameterized values for the user to specify @Ramya, these need to be 
 start_date = "2002-01-01"  # need to get as input from webpage, might need to cast to a string
 end_date = "2008-02-01"  # need to get as input from webpage, might need to cast to a string
 hit_threshold = '"Platinum"@en'
+
+"""
+VC TO DO: Add an if statement here and a checkbox variable to include featured artists in the results
+if Checked add query_dbpedia_isHitSongOf to the list, if not add query_is_primary_song_artist 
+
+NOTE: IN the return set you will also need to chaneg the variable to get the correct data from 
+Fuseki and display on the site. 
+"""
 
 # VC - constructs a triple in the format  ?song isHitSongOf ?artist
 query_dbpedia_isHitSongOf = ('CONSTRUCT {?works <http://example.org/isHitSongOf> ?artist} '
@@ -31,11 +40,38 @@ query_foaf_artist_name = (prefixes +
                           '&& ?releaseDate < "' + end_date + '"^^xsd:date)}')
 external_ontology_queries.append(query_foaf_artist_name)
 
-# VC - Need a query that returns the primary artis of a song. Can be done using
 """
-if song belongs to an album use that > IE primary artist is the one who recorded the album
-else, other idea, some other source have maybe some type of property? that defines this
-What about songs that don't belong to any album? billing order on the listing?
+VC - if song belongs to an album using that to determine the primary artist of a song. This, in effect, removes
+the featured artist as the one who has the hit. We can resuse this query in other places as well. 
 """
 
+query_is_primary_song_artist =('CONSTRUCT{?works <http://example.org/isHitSongOfPrimaryArtist> ?album_artist} '
+                               'WHERE {?works a dbo:Song; '
+                               'dbp:award ' + hit_threshold + '; ' +
+                               'dbo:artist ?artist_uri; '
+                               'dbo:releaseDate ?releaseDate; '
+                               'dbo:album ?works_album. '
+                               '?works_album dbp:artist ?album_artist. '
+                               '?album_artist a owl:Thing. '
+                               'FILTER( ?releaseDate > "' + start_date + '"^^xsd:date '
+                               '&& ?releaseDate < "' + end_date + '"^^xsd:date)}')
 
+external_ontology_queries.append(query_is_primary_song_artist)
+
+
+"""
+Here are the queries that return results from Fuseki and they can be used on our front-end
+"""
+return_all_isHitSongOf_artists = (
+    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+    "PREFIX ex: <http://example.org/> "
+    "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
+    "SELECT DISTINCT ?artist_name WHERE { "
+    "?song ex:isHitSongOf ?artist. "
+    "?artist foaf:name ?artist_name "
+    "}"
+)
+return_Fuseki_results_queries.append(return_all_isHitSongOf_artists)
+
+return_all_isHitSongOfPrimaryArtist_artists = "temp"
