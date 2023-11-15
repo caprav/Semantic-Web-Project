8e8a645d789661ce98b41a8e9ceb76359c1bb36e
caprav <vines07@gmail.com>
2023-11-15 11:44:15 -0500

    Some updates to test federated query, but not working yet

--- hits_by_song_duration.py
+++ hits_by_song_duration.py
@@ -4,10 +4,13 @@ from two_hit_wonders import query_dbpedia_isHitSongOf
 duration_external_ontology_queries = []
 duration_return_Fuseki_results_queries = []
 
-prefixes = ("prefix foaf: <http://xmlns.com/foaf/0.1/> "
-            "prefix owl:  <http://www.w3.org/2002/07/owl#> "
-            "PREFIX mo: <http://purl.org/ontology/mo/> "
-            "")
+prefixes = ("prefix foaf:   <http://xmlns.com/foaf/0.1/> "
+            "prefix owl:    <http://www.w3.org/2002/07/owl#> "
+            "PREFIX mo:     <http://purl.org/ontology/mo/> "
+            "PREFIX p:      <http://www.wikidata.org/prop/> "
+            "PREFIX wd:     <http://www.wikidata.org/entity/> "
+            "PREFIX wds:    <http://www.wikidata.org/entity/statement/> "
+            )
 
 duration_start_date = "2007-01-01"  # need to get as input from webpage, might need to cast to a string
 duration_end_date = "2007-03-01"  # need to get as input from webpage, might need to cast to a string
@@ -23,8 +26,10 @@ query_dbpedia_sameAs_song = ('CONSTRUCT {?works owl:sameAs ?external_work} '
                              'FILTER( ?releaseDate > "' + duration_start_date + '"^^xsd:date '
                              '&& ?releaseDate < "' + duration_end_date + '"^^xsd:date)}')
 
-query_mbrainz_duration = ('CONSTRUCT {?external_work <http://example.org/hasDuration> ?duration} '
-                          'WHERE {?external_work mo:length ?duration. '
+query_wikidata_country_of_origin = (prefixes +
+                          'CONSTRUCT {?external_work <http://example.org/isAHitSongFrom> ?country} '
+                          # VC - p:P495 = country of origin https://www.wikidata.org/wiki/Property:P495
+                          'WHERE {?external_work p:P495 ?country. '                           
                           'SERVICE <https://dbpedia.org/sparql> {'
                           '?works a dbo:Song; '
                           'dbp:award ' + duration_hit_threshold + '; ' +
@@ -33,6 +38,17 @@ query_mbrainz_duration = ('CONSTRUCT {?external_work <http://example.org/hasDura
                           'FILTER( ?releaseDate > "' + duration_start_date + '"^^xsd:date '
                           '&& ?releaseDate < "' + duration_end_date + '"^^xsd:date)}}')
 
+createJustlittleTestQueryForwikidata = (prefixes +
+                          'CONSTRUCT {?external_work <http://example.org/isAHitSongFrom> ?country} '
+                          # VC - p:P495 = country of origin https://www.wikidata.org/wiki/Property:P495
+                          'WHERE {?external_work p:P495 ?country. '         # :P361 - PartOf                  
+                          '} LIMIT 100')
+
+
+
+# dbp:label # like the label columbia from the dbpedia link
+
 #  duration_external_ontology_queries.append(query_dbpedia_isHitSongOf)
 #  duration_external_ontology_queries.append(query_dbpedia_sameAs_song)
-duration_external_ontology_queries.append(query_mbrainz_duration)
+#  duration_external_ontology_queries.append(query_wikidata_country_of_origin)
+duration_external_ontology_queries.append(createJustlittleTestQueryForwikidata)
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # sk-Import necessary libraries and modules
 import os
+import urllib
 from flask_migrate import Migrate
 from flask_minify import Minify
 from sys import exit
@@ -10,13 +11,12 @@ from SPARQLWrapper import SPARQLWrapper, JSON, N3
 from pprint import pprint
 from two_hit_wonders import (
     external_ontology_queries, return_all_isHitSongOf_artists
-)  #  query_dbpedia_isHitSongOf, query_foaf_artist_name
+)
 from hits_by_song_duration import duration_external_ontology_queries
 from flask import Flask, render_template, request
 from apps.config import config_dict
 from apps import create_app, db
 import jsonify
-
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
@@ -48,44 +48,107 @@ if DEBUG:
 # Sharayu: Define a route to interact with Fuseki and DBpedia
 @app.route("/get_fuseki", methods=["POST"])
 def fuseki():
+    global tempResult
     print("inside fuseki")
-
-    # query1= request.form['query']
-
-    # Sharayu: Define SPARQL query to retrieve data from DBpedia
-
-    sparql = SPARQLWrapper("https://dbpedia.org/sparql")
-    sparql.setQuery(
-        """
-        SELECT ?artist ?birthplace
-    WHERE {
-    ?artist a dbo:MusicalArtist ;
-            dbo:birthPlace ?birthplace .
-    }
-    LIMIT 10
-    """
-    )
-    sparql.setReturnFormat(JSON)
-    #qres = sparql.query().convert()
 
     store = SPARQLUpdateStore()
     query_endpoint = "http://localhost:3030/music/query"
     update_endpoint = "http://localhost:3030/music/update"
     store.open((query_endpoint, update_endpoint))
 
-
-    sparql = SPARQLWrapper("http://dbtune.org/musicbrainz/snorql/")  # "https://dbpedia.org/sparql")
+    querysite = 'wikidata' # wikidata or dbpedia
+    duration_hit_threshold = '"Platinum"@en'
+    duration_start_date = "2007-01-01"
+    duration_end_date = "2007-03-01"
+    prefixes = ("prefix foaf:   <http://xmlns.com/foaf/0.1/> "
+                "prefix owl:    <http://www.w3.org/2002/07/owl#> "
+                "prefix dbo:    <http://dbpedia.org/ontology/> "
+                "prefix dbp:    <http://dbpedia.org/property/> "
+                "PREFIX mo:     <http://purl.org/ontology/mo/> "
+                "PREFIX p:      <http://www.wikidata.org/prop/> "
+                "PREFIX ps:     <http://www.wikidata.org/prop/statement/> "
+                "PREFIX wd:     <http://www.wikidata.org/entity/> "
+                "PREFIX wds:    <http://www.wikidata.org/entity/statement/> "
+                )
 
     g = Graph(store, identifier=default)
+    match querysite:
+        case'wikidata':
+            print('Querying wikiData')
+            sparql = SPARQLWrapper("https://query.wikidata.org/")
+            #"https://query.wikidata.org/")  # "https://dbpedia.org/sparql"
 
-    for query in duration_external_ontology_queries: # external_ontology_queries:
-        sparql.setQuery(query)
-        sparql.setReturnFormat(N3)
-        query_result = sparql.query().convert()
-        g.parse(query_result)
+            wikidataEndPointUrl = 'https://query.wikidata.org/sparql'
+
+
+            tempQuery1 = (prefixes +
+                         'CONSTRUCT {?external_work <http://example.org/isAHitSongFrom> ?country} '
+                         # VC - p:P495 = country of origin https://www.wikidata.org/wiki/Property:P495
+                         'WHERE {?external_work p:P495 ?statement. '
+                         '?statement ps:P495 ?country '  # :P361 - PartOf                  
+                         '} LIMIT 100')
+
+            tempQuery2 = (prefixes +
+                         'CONSTRUCT {?external_work_URI <http://example.org/isAHitSongFrom> ?country} '
+                         # VC - p:P495 = country of origin https://www.wikidata.org/wiki/Property:P495
+                         'WHERE {?external_work_URI p:P495 ?statement. '
+                         '?statement ps:P495 ?country '
+                         '{SERVICE <https://dbpedia.org/sparql> { '
+                         'SELECT ?external_work_URI WHERE{'
+                         '?works a dbo:Song; '
+                         'dbp:award ' + duration_hit_threshold + '; ' +
+                         'dbo:releaseDate ?releaseDate; '
+                         'owl:sameAs ?external_work_URI. '
+                         'FILTER( ?releaseDate > "' + duration_start_date + '"^^xsd:date '
+                                '&& ?releaseDate < "' + duration_end_date + '"^^xsd:date '
+                                '&& (isUri(?external_work_URI) || isIri(?external_work_URI)) '
+                                '&& STRSTARTS(STR(?external_work_URI), STR(wd:))) '                                       
+                         '}}}}'
+                         )
+            print(tempQuery2)
+            resultURL = wikidataEndPointUrl + '?query=' + urllib.parse.quote(tempQuery2)
+            print(resultURL)
+            tempResult = g.parse(location=resultURL)
+
+        case _:
+            print('Querying DBPedia')
+            sparql = SPARQLWrapper("https://dbpedia.org/sparql")
+
+            dbpediaquery= (prefixes +
+                          'SELECT ?external_work_URI '
+                          # VC - p:P495 = country of origin https://www.wikidata.org/wiki/Property:P495
+                          'WHERE {'
+                          '?works a dbo:Song; '
+                          'dbp:award ' + duration_hit_threshold + '; ' +
+                          'dbo:releaseDate ?releaseDate; '
+                          'owl:sameAs ?external_work_URI. ' 
+                          'FILTER( ?releaseDate > "' + duration_start_date + '"^^xsd:date '
+                          '&& ?releaseDate < "' + duration_end_date + '"^^xsd:date '
+                          '&& (isUri(?external_work_URI)) && STRSTARTS(STR(?external_work_URI), STR(wd:))) }')
+            print(dbpediaquery)
+            sparql.setQuery(dbpediaquery)
+            sparql.setReturnFormat(N3)
+            query_result = sparql.query().convert()
+            print(query_result)
+
+            """
+            for query in duration_external_ontology_queries:
+                # duration_external_ontology_queries: # external_ontology_queries:
+                sparql.setQuery(query)
+                sparql.setReturnFormat(N3)
+                print('first')
+                query_result = sparql.query().convert()
+                print('second')
+                print(query_result)
+                print('third')
+                # g.parse(query_result)
+            """
+            tempResult = g.parse(query_result)
+
+    print(tempResult)
 
     #  VC - This line actually puts the info into Fuseki
-    store.add_graph(g)
+    store.add_graph(tempResult)        #g)
 
     #  VC - can't use sparql below, need to use the store (pointing to Fuseki), sparql is pointing to dbpedia
     fuseki_result = store.query(return_all_isHitSongOf_artists)
@@ -99,7 +162,6 @@ def fuseki():
     print(res)
 
     return render_template("fedrated.html", query_result=res)
-
 
 if __name__ == "__main__":
     # Change the port to the desired value (e.g., 8080)
