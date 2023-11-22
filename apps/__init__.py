f9275752e8650ed09fe4bdf5ced0d00b7b0b6f81
Sharayu <sharayu@Gauravs-MacBook-Pro-2.local>
2023-11-22 12:38:34 -0500

    Adding sales report backend code

--- apps/__init__.py
+++ apps/__init__.py
@@ -15,7 +15,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home','history','reviews','fuseki'):
+    for module_name in ('authentication', 'home','history','reviews','fuseki','dbpedia'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
