9b6d3d37cea51556ec845306ffd730c3b54ae97b
caprav <vines07@gmail.com>
2023-11-26 14:04:03 -0500

    Fixes issues with Query 3

--- hits_by_country.py
+++ hits_by_country.py
@@ -54,13 +54,16 @@ class hits_by_country_queries:
         store.open((query_endpoint, update_endpoint))
         print("store is open")
         results_federated_query = (self.prefixes +
-            "SELECT DISTINCT ?external_work_URI ?country "
+            "SELECT DISTINCT ?external_work_URI ?label "
             "WHERE { "
             "?dbpedia_work owl:sameAs ?external_work_URI; "
             "dbo:releaseDate ?releaseDate. "
             "SERVICE <https://query.wikidata.org/sparql> { "
             "?external_work_URI p:P495 ?statement. "
             "?statement ps:P495 ?country. "
+            "?country rdfs:label ?label "
+            #"?native_label ps:P1705 ?country_name "
+            "FILTER(lang(?label) = 'en') "
             "} "
             'FILTER( ?releaseDate > "' + str(self.start_date) + '"^^xsd:date && ?releaseDate < "' + str(self.end_date) + '"^^xsd:date )'
             "} "
@@ -76,10 +79,12 @@ class hits_by_country_queries:
         print(3)
         for record in fuseki_result:
             #  VC - this should get the portion returned by the SELECT ?country in results_federated_query above
-            temp_country = record.country.toPython()
+            temp_country = record.label.toPython()
             print(4)
+            print(temp_country)
             #  VC - https://www.geeksforgeeks.org/python-increment-value-in-dictionary/
             return_result_dictionary[temp_country] = return_result_dictionary.get(temp_country, 0) + 1
         print(5)
         #  VC -return an array of key/value pairs "country, count"
+        print(return_result_dictionary)
         return return_result_dictionary
